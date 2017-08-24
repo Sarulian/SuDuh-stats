4e529f3a4c680f87bd4bb7a07ab6191a6e7448a5
Sarulian <tedjamesrobinson@gmail.com>
2017-08-24 16:16:00 -0700

    old config

---
+++ config.py
@@ -0,0 +1,10 @@
+api_base = "https://na1.api.riotgames.com/lol/"
+
+key = "RGAPI-d2394dfd-bf06-4a2f-8e43-73a5f553b47d"
+
+summoner_ids = {
+					"troze": "207516619",
+					"ibrosky": "205030904",
+					"deckonthedeck": "207521602",
+					"pippey": "206633197"
+				}
